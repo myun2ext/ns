68ae35e3697f11eafcd822be66f4986677215afa
myun2 <myun2@nwhite.info>
2015-07-08 15:19:09 +0900

    ns add (init)

---
+++ nscript.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "nscript.h"
+
+int main()
+{
+	return 0;
+}
---
+++ nscript.hpp
@@ -0,0 +1,11 @@
+#ifndef __MYUN2_GITHUB_COM__NS_HPP__
+#define __MYUN2_GITHUB_COM__NS_HPP__
+
+namespace myun2
+{
+	namespace ns
+	{
+	}
+}
+
+#endif///__MYUN2_GITHUB_COM__NS_HPP__
