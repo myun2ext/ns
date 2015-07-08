f2cdb08da621beda6017dadb7f7757aafa5509b2
myun2 <myun2@nwhite.info>
2015-07-08 16:45:37 +0900

    implement2

--- nscript.cpp
+++ nscript.cpp
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "nscript/loader.hpp"
+#include "nscript/tokenizer.hpp"
 #include "nscript.hpp"
 
 using namespace myun2::nscript;
 
 int main()
 {
+	//	Load phase
 	loader l("ex.txt");
 	puts(l.data());
+
+	//	Parse phase
+	tokenizer tk;
+	tokenizer::token_list tl = tk.parse(l.data());
+
+	for(int i=0; i<tl.size(); i++)
+		printf("\"%s\"\n", tl[i].c_str());
 	return 0;
 }
---
+++ nscript/tokenizer.hpp
@@ -0,0 +1,51 @@
+#ifndef __MYUN2_GITHUB_COM__NSCRIPT__TOKENIZER_HPP__
+#define __MYUN2_GITHUB_COM__NSCRIPT__TOKENIZER_HPP__
+
+#include <string>
+#include <vector>
+
+namespace myun2
+{
+	namespace nscript
+	{
+		class tokenizer
+		{
+		public:
+			typedef ::std::vector< ::std::string> token_list_type, token_list;
+			token_list parse(const char* data)
+			{
+				token_list_type tokens;
+				const char* p = data;
+				const char* token_started = data;
+				char c;
+
+				while((c = *p) != '\0')
+				{
+					if ( is_split_char(c) || is_symbol_char(c) ) {
+						unsigned long length = p - token_started;
+						if ( length != 0 ) {
+							::std::string token(token_started, length);
+							tokens.push_back(token);
+							token_started = p;
+						}
+					}
+					p++;
+				}
+				return tokens;
+			}
+			bool is_split_char(char c) {
+				if ( c == ' ' || c == '\t' || c == '\r' || c == '\n' ) return true;
+				return false;
+			}
+			bool is_symbol_char(char c) {
+				if ( c > ' ' && c < '0' ) return true;
+				if ( c > '9' && c < 'A' ) return true;
+				if ( c > 'Z' && c < 'a' && c != '_' ) return true;
+				if ( c > 'z' && c <= '~' ) return true;
+				return false;
+			}
+		};
+	}
+}
+
+#endif///__MYUN2_GITHUB_COM__NSCRIPT__TOKENIZER_HPP__
